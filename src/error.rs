33e6971f33b687a06f3c52a8d46a7bff7c6b6b02
Leif Grele <lkgrele@us.ibm.com>
2016-07-13 13:35:36 -0400

    Use a custom error type
    This is just a quick-and-dirty error implementation
    to allow error conversion from library errors.
    It would be a good idea to replace it with a better
    implementation in the future.

---
+++ src/error.rs
@@ -0,0 +1,51 @@
+extern crate serde_json;
+extern crate hyper;
+extern crate hubcaps;
+
+use std::error::Error;
+use std::fmt;
+
+use hyper::error::Error as HypErr;
+use serde_json::error::Error as SjErr;
+use hubcaps::errors::Error as HubErr;
+
+#[derive(Debug)]
+pub struct SteveError {
+    error_message: String,
+}
+
+impl Error for SteveError {
+    fn description(&self) -> &str {
+        self.error_message.as_str()
+    }
+}
+
+impl fmt::Display for SteveError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self.error_message)
+    }
+}
+
+impl From<HypErr> for SteveError {
+    fn from(err: HypErr) -> Self {
+        SteveError { error_message: format!("{:?}", err) }
+    }
+}
+
+impl From<SjErr> for SteveError {
+    fn from(err: SjErr) -> Self {
+        SteveError { error_message: format!("{:?}", err) }
+    }
+}
+
+impl From<HubErr> for SteveError {
+    fn from(err: HubErr) -> Self {
+        SteveError { error_message: format!("{:?}", err) }
+    }
+}
+
+impl From<String> for SteveError {
+    fn from(err: String) -> Self {
+        SteveError { error_message: err }
+    }
+}
